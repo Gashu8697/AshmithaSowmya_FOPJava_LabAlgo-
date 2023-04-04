bbe82f1694da1989ec7fcbe24937bea12cfeb012
Ashmitha Sowmya <76016259+Gashu8697@users.noreply.github.com>
2023-04-04 12:19:03 +0530

    Add files via upload

---
+++ NoteCount.java
@@ -0,0 +1,28 @@
+package com.algorithms.Labassignment1;
+
+public class NoteCount {
+
+	public void counting(int[] currency, int amount) {
+
+		int noteCount[] = new int[currency.length];
+		int i;
+		//int sumofnotes = 0;
+		for (i = 0; i < currency.length; i++) {
+			noteCount[i] = amount / currency[i];
+			amount = amount % currency[i];
+			//sumofnotes += noteCount[i];
+		}
+		System.out.println("Your payment approach in order to give min no of notes will be");
+		for (i = 0; i < currency.length; i++) {
+
+			if (noteCount[i] != 0) {
+
+				System.out.println(currency[i] + ":" + noteCount[i]);
+			}
+		}
+
+		// System.out.println(" "+sumofnotes);
+
+	}
+
+}
